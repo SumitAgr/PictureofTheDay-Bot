f71b8d5a70e91723d304f555155683eeff4f3c33
SumitAgr <sumit.r.agr@gmail.com>
2019-12-29 03:45:41 -0500

    10 minute cooldown logic applied

--- bot.py
+++ bot.py
@@ -14,10 +14,13 @@ import config
 
 # Import time library
 import time
-import datetime
+from datetime import timedelta, datetime
+from pytz import timezone
+
+# Using US/Eastern time
+est_timezone = timezone('US/Eastern')
 
 # TinyDB 
-
 from tinydb import TinyDB, Query
 db = TinyDB('db.json')
 
@@ -40,6 +43,9 @@ nasa_url = 'https://api.nasa.gov/planetary/apod?api_key={}'.format(nasa_api_key)
 reply_keyboard = [['/picture 🖼']]
 markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard = True)
 
+# DateTime format to use everywhere
+fmt = '%Y-%m-%d %H:%M:%S'
+
 # Typing animation to show to user to imitate human interaction
 def send_action(action):
     def decorator(func):
@@ -56,7 +62,7 @@ send_typing_action = send_action(ChatAction.TYPING)
 # '/start' command
 @send_typing_action
 def start(bot, update):
-    bot.send_message(chat_id = update.message.chat_id, text = "Hello there! Thank you for starting me! Use the /picture command to see today's NASA image of the day!", reply_markup = markup)
+    bot.send_message(chat_id = update.message.chat_id, text = "Hello! Thank you for starting me! Use the /picture command to see today's NASA Image of the Day!", reply_markup = markup)
 
     print(datetime.datetime.now())
     print("User {} and ID {} started the bot!".format(update.message.chat_id, str(update.message.from_user.username)))
@@ -68,35 +74,70 @@ dispatcher.add_handler(start_handler)
 @send_typing_action
 def pictureoftheday_message(bot, update):
 
+    # A new user has invoked the picture command since the chat_id cannot be found in database
     if db.contains(Query()['chat_id'] == update.message.chat_id) == False:
-        db.insert({'chat_id': update.message.chat_id, 'time': str(datetime.datetime.now())})
+        db.insert({'chat_id': update.message.chat_id, 'time': str(datetime.now(est_timezone).strftime(fmt))})
+
+        nasa_data = requests.get(nasa_url).json()
+        title = nasa_data['title']
+        explanation = nasa_data['explanation']
+        if 'image' in nasa_data['media_type']:
+            image = nasa_data['hdurl']
+            bot.send_message(chat_id = update.message.chat_id, text = title)
+            bot.send_photo(chat_id = update.message.chat_id, photo = image)
+            bot.send_message(chat_id = update.message.chat_id, text = explanation)
+            print("A new user {} and ID {} called the /picture command!".format(update.message.chat_id, str(update.message.from_user.username)))
+        elif 'video' in nasa_data['media_type']:
+            video = nasa_data['url']
+            bot.send_message(chat_id = update.message.chat_id, text = title)
+            bot.send_message(chat_id = update.message.chat_id, text = video)
+            bot.send_message(chat_id = update.message.chat_id, text = explanation)
+            print("A new user {} and ID {} called the /picture command!".format(update.message.chat_id, str(update.message.from_user.username)))
+        else:
+            bot.send_message(chat_id = update.message.chat_id, text = "Sorry, I couldn't deliver the image / video! An error occured!")
+            print("A new user {} and ID {} called the /picture command and an error occured!".format(update.message.chat_id, str(update.message.from_user.username)))
+    # If user exists, search the last time they invoked the picture command
+    elif db.contains(Query()['chat_id'] == update.message.chat_id) == True:
+        user = Query()
+        result = db.search((user.chat_id == update.message.chat_id) & (user.time != str(0)))
+        time_getter = [sub['time'] for sub in result]
+        print(time_getter[0])
+
+        old_time = datetime.strptime(str(time_getter[0]), fmt)
+        current_time = datetime.strptime(str(datetime.now(est_timezone).strftime(fmt)), fmt)
+
+        # Calculate how much time has passed since we served the image
+        minutes_diff = (current_time - old_time).total_seconds() / 60.0
+
+        # If more than 10 minutes have passed, they can reuse the command
+        if int(minutes_diff) >= 10:
+            db.upsert({'time': str(datetime.now(est_timezone).strftime(fmt))}, Query()['chat_id'] == update.message.chat_id)
+
+            nasa_data = requests.get(nasa_url).json()
+            title = nasa_data['title']
+            explanation = nasa_data['explanation']
+            if 'image' in nasa_data['media_type']:
+                image = nasa_data['hdurl']
+                bot.send_message(chat_id = update.message.chat_id, text = title)
+                bot.send_photo(chat_id = update.message.chat_id, photo = image)
+                bot.send_message(chat_id = update.message.chat_id, text = explanation)
+                print("User {} and ID {} called the /picture command!".format(update.message.chat_id, str(update.message.from_user.username)))
+            elif 'video' in nasa_data['media_type']:
+                video = nasa_data['url']
+                bot.send_message(chat_id = update.message.chat_id, text = title)
+                bot.send_message(chat_id = update.message.chat_id, text = video)
+                bot.send_message(chat_id = update.message.chat_id, text = explanation)
+                print("User {} and ID {} called the /picture command!".format(update.message.chat_id, str(update.message.from_user.username)))
+            else:
+                bot.send_message(chat_id = update.message.chat_id, text = "Sorry, I couldn't deliver the image / video! An error occured!")
+                print("User {} and ID {} called the /picture command and an error occured!".format(update.message.chat_id, str(update.message.from_user.username)))
+        else:
+            bot.send_message(chat_id = update.message.chat_id, text = "You're doing that too much. Please try again in {} minute(s)!".format(10 - int(minutes_diff)))
+            print("User {} and ID {} spammed the /picture command and hit a cooldown!".format(update.message.chat_id, str(update.message.from_user.username)))
     else:
-        db.upsert({'time': str(datetime.datetime.now())}, Query()['chat_id'] == update.message.chat_id)
+        pass
 
-    #difference = last_time -
-
-    user = Query()
-    result = db.search((user.chat_id == update.message.chat_id) & (user.time != str(0)))
-    time_getter = [sub['time'] for sub in result]
-    print(time_getter[0])
-    
-    nasa_data = requests.get(nasa_url).json()
-    title = nasa_data['title']
-    explanation = nasa_data['explanation']
-    if 'image' in nasa_data['media_type']:
-        image = nasa_data['hdurl']
-        bot.send_message(chat_id = update.message.chat_id, text = title)
-        bot.send_photo(chat_id = update.message.chat_id, photo = image)
-        bot.send_message(chat_id = update.message.chat_id, text = explanation)
-    elif 'video' in nasa_data['media_type']:
-        video = nasa_data['url']
-        bot.send_message(chat_id = update.message.chat_id, text = title)
-        bot.send_message(chat_id = update.message.chat_id, text = video)
-        bot.send_message(chat_id = update.message.chat_id, text = explanation)
-    else:
-        bot.send_message(chat_id = update.message.chat_id, text = "Sorry, I couldn't deliver the image / video! An error occured!")
-
-    print(datetime.datetime.now())
+    print(datetime.now(est_timezone).strftime(fmt))
     print("User {} and ID {} called the /picture command!".format(update.message.chat_id, str(update.message.from_user.username)))
 
 pictureoftheday_message_handler = CommandHandler('picture', pictureoftheday_message)
@@ -105,10 +146,10 @@ dispatcher.add_handler(pictureoftheday_message_handler)
 # Unknown command for error handling
 @send_typing_action
 def unknown(bot, update):
-    bot.send_message(chat_id = update.message.chat_id, text="Sorry, I didn't understand that command! Please try again!")
+    bot.send_message(chat_id = update.message.chat_id, text="Sorry, I didn't understand that command! Please type /picture!")
 
-    print(datetime.datetime.now())
-    print("User {} called an unknown command!".format(update.message.chat_id))
+    print(datetime.now(est_timezone))
+    print("User {} and ID {} called an unknown command!".format(update.message.chat_id, str(update.message.from_user.username)))
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
